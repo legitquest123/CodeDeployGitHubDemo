aa755eff50e2378882904f418da9028c47e62d45
legitquest123 <namit.singh@legitquest.com>
2020-04-07 14:48:53 +0530

    7-April-2020 Seven Changes

--- lqsite/app/Http/Controllers/NotesController.php
+++ lqsite/app/Http/Controllers/NotesController.php
@@ -78,7 +78,7 @@ class NotesController extends Controller
     		$view .= '<h4 class="h4 card-title d-block mb-1 font-weight-bold" >'.$value['title'].'</h4>
                         <hr class="mb-3">
                         <div class="row mb-2">
-                          <div style="height:100%; border-style:none;" class="col-sm-12 b-b">
+                          <div style="height:100%; border-style:none; text-align:justify;" class="col-sm-12 b-b">
                             <div class="label" style="font-size:15px; font-weight:bold; padding:0px 0px 10px 0px;"></div>
                             
                             <h4 style="padding:6px 0px 10px 0px;">'.$value['title'].'</h4>
@@ -131,7 +131,7 @@ class NotesController extends Controller
     		$view .= '<h4 class="h4 card-title d-block mb-1 font-weight-bold" >'.$value['title'].'</h4>
                         <hr class="mb-3">
                         <div class="row mb-2">
-                          <div class="col-sm-12 b-b">
+                          <div style="text-align:justify;" class="col-sm-12 b-b">
                             <div class="label" style="font-size:15px; font-weight:bold; padding:0px 0px 10px 0px;"></div>
                             <h4 style="padding:6px 0px 10px 0px;">'.$value['title'].'</h4>
                             <p style="font-size:13px; text-align:justify;">'.$value['description'].'</p>
